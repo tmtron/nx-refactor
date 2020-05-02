18594d4ddb147f06fac8eb6f9c106fe50fdb1ca5
TmTron <martin.trummer@tmtron.com>
2020-05-02 10:13:26 +0200

    added a node app

---
+++ apps/app/src/main.ts
@@ -0,0 +1,3 @@
+import {LIB_A} from "@nx-refactor/lib-a";
+
+console.log('constant value = '+LIB_A);
--- libs/lib-a/src/lib/lib-a.ts
+++ libs/lib-a/src/lib/lib-a.ts
@@ -2,4 +2,4 @@ import {LIB_B} from "@nx-refactor/lib-b";
 import {LIB_C} from "@nx-refactor/lib-c";
 
 
-export const LIB_A = LIB_B + LIB_C;
+export const LIB_A = LIB_B +' - ' + LIB_C;
