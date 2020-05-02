599a72856d09c909112263ce0a79a2cb70d0ed84
TmTron <martin.trummer@tmtron.com>
2020-05-02 10:15:44 +0200

    removed lib-c

--- libs/lib-a/src/lib/lib-a.ts
+++ libs/lib-a/src/lib/lib-a.ts
@@ -1,5 +1,4 @@
 import {LIB_B} from "@nx-refactor/lib-b";
-import {LIB_C} from "@nx-refactor/lib-c";
 
 
-export const LIB_A = LIB_B +' - ' + LIB_C;
+export const LIB_A = 'lib-a : ' + LIB_B ;
--- libs/lib-c/src/lib/lib-c.ts
+++
@@ -1,1 +0,0 @@
-export const LIB_C = 'lib-c';
